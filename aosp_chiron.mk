79de9982ec8643585a84ffb3a6f5f579b278c67f
Huexxx <huexxx@gmail.com>
2022-02-20 16:44:17 +0100

    PixelExperientize and update FINGERPRINT to SQ1D.220205.004 from Pixel 6 Pro

--- aosp_chiron.mk
+++ aosp_chiron.mk
@@ -21,8 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from chiron device
 $(call inherit-product, device/xiaomi/chiron/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common PixelExperience stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Lawnchair
+-include vendor/huexxx-prebuilts/packages/apps/Lawnchair/lawnchair.mk
+-include vendor/huexxx-prebuilts/packages/apps/Lawnicons/lawnicons.mk
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_NAME := aosp_chiron
 PRODUCT_DEVICE := chiron
@@ -32,8 +39,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
+# Fingerprint
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.004/8151327:user/release-keys
+BUILD_DESCRIPTION := raven-user 12 SQ1D.220205.004 8151327 release-keys
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
-PRODUCT_OVERRIDE_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
