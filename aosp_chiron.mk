724c4f032b1867eab5fd6606960ad21220482874
Huexxx <huexxx@gmail.com>
2022-02-20 16:44:17 +0100

    Arcanize and update FINGERPRINT to SQ1D.220205.004 from Pixel 6 Pro

--- aosp_chiron.mk
+++ aosp_chiron.mk
@@ -17,12 +17,26 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from chiron device
 $(call inherit-product, device/xiaomi/chiron/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Project Arcana stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/themes.mk)
+
+# Repainter integration
+PRODUCT_PACKAGES += \
+    RepainterServicePriv
+
+ARCANA_DEVICE := chiron
+ARCANA_MAINTAINER := Huexxx
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+TARGET_SUPPORTS_BLUR := false
+USE_PIXEL_CHARGER_IMAGES := true
+WITH_GAPPS := false
 
 PRODUCT_NAME := aosp_chiron
 PRODUCT_DEVICE := chiron
@@ -32,8 +46,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
+# Fingerprint
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.004/8151327:user/release-keys
+BUILD_DESCRIPTION := raven-user 12 SQ1D.220205.004 8151327 release-keys
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
-PRODUCT_OVERRIDE_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
