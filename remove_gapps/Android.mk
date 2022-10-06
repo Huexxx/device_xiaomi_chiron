b08e3ed16b6401eb7631a53e0f4cd064997e9d1b
Huexxx <huexxx@gmail.com>
2022-10-06 20:29:07 +0200

    fix gapps_remover

--- remove_gapps/Android.mk
+++ remove_gapps/Android.mk
@@ -5,67 +5,52 @@ LOCAL_MODULE := RemoveGapps
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    AmbientStreaming \
-    AppDirectedSMSService \
-    arcore \
-    BetterBugStub \
-    CaptivePortalLoginGoogle \
     CarrierMetrics \
-    CarrierSetup \
-    CarrierWifi \
-    CbrsNetworkMonitor \
-    ConnectivityThermalPowerManager \
-    ConnMO \
-    ConnMetrics \
-    DCMO \
     DevicePolicyPrebuilt \
     DiagnosticsToolPrebuilt \
-    DMService \
-    Drive \
-    GCS \
-    grilservice \
     GoogleTTS \
-    KidsSupervisionStub \
-    Maps \
-    MlkitBarcodeUIPrebuilt \
-    Music \
-    MyVerizonServices \
-    NetworkPermissionConfigGoogle \
-    NetworkStackGoogle \
-    NgaResources \
-    OBDM_Permissions \
-    obdm_stub \
-    OdadPrebuilt \
-    OemDmTrigger \
-    OPScreenRecord \
-    Ornament \
     Photos \
-    PixelDependencies \
     PlayAutoInstallConfig \
-    PrebuiltGoogleAdservicesTvp \
-    PrebuiltGoogleTelemetryTvp \
+    Tycho \
+    VZWAPNLib \
+    arcore \
+    talkback \
+    AmbientStreaming \
+    AppDirectedSMSService \
+    BetterBugStub \
+    CarrierLocation \
+    CarrierServices \
+    CarrierWifi \
+    CbrsNetworkMonitor \
+    DCMO \
+    ConnMO \
+    DMService \
+    DreamlinerPrebuilt \
+    DreamlinerUpdater \
+    GCS \
+    GoogleCamera \
+    KidsSupervisionStub \
+    ImsServiceEntitlement \
+    MaestroPrebuilt \
+    MyVerizonServices \
+    OemDmTrigger \
+    OdadPrebuilt \
     RecorderPrebuilt \
-    RilConfigService \
     SCONE \
+    SafetyHubPrebuilt \
     ScribePrebuilt \
     SecurityHubPrebuilt \
     Showcase \
-    SoundAmplifierPrebuilt \
-    SprintDM \
-    SprintHM \
-    StorageManagerGoogle \
-    talkback \
     TetheringEntitlement \
-    TurboAdapter \
     TurboPrebuilt \
-    Tycho \
-    USCCDM \
-    Videos \
-    VisionBarcodePrebuilt \
-    VZWAPNLib \
     VzwOmaTrigger \
-    YouTube \
-    YouTubeMusicPrebuilt
+    WfcActivation \
+    DocumentsUIGoogle \
+    CarrierSetup \
+    ConnectivityThermalPowerManager \
+    grilservice \
+    RilConfigService \
+    TurboAdapter
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
