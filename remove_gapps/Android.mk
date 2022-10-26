29b52ff548c539c84e8113020c9ec415c5e0b470
Huexxx <huexxx@gmail.com>
2022-10-26 17:22:07 +0200

    chiron: Support GoogleBattery

--- remove_gapps/Android.mk
+++ remove_gapps/Android.mk
@@ -47,8 +47,7 @@ LOCAL_OVERRIDES_PACKAGES := \
     CarrierSetup \
     ConnectivityThermalPowerManager \
     grilservice \
-    RilConfigService \
-    TurboAdapter
+    RilConfigService
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
