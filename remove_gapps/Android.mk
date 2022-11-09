d0a92d86d69a133a9fb9959680450d8cd4bad049
Huexxx <huexxx@gmail.com>
2022-11-09 11:27:54 +0100

    Align RemoveGapps with other projects

--- remove_gapps/Android.mk
+++ remove_gapps/Android.mk
@@ -5,20 +5,20 @@ LOCAL_MODULE := RemoveGapps
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-     TurboAdapter
+    DreamlinerPrebuilt \
+    DreamlinerUpdater \
+    GoogleCamera \
+    SCONE \
+    AmbientStreaming \
+    OemDmTrigger \
+    Tycho \
+    VZWAPNLib \
+    grilservice \
+    ConnectivityThermalPowerManager \
+    MyVerizonServices \
+    RilConfigService \
+    PlayAutoInstallConfig
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
-
-#    DevicePolicyPrebuilt \
-#    GoogleTTS \
-#    Photos \
-#    arcore \
-#    talkback \
-#    CarrierServices \
-#    ImsServiceEntitlement \
-#    SafetyHubPrebuilt \
-#    ScribePrebuilt \
-#    TurboPrebuilt \
-#    CarrierSetup \
