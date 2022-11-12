07a1d275422a2a3d725761472cc7ff77204547a2
Huexxx <huexxx@gmail.com>
2022-11-12 16:02:57 +0100

    chiron: vendor_prop.mk -> vendor.prop

--- vendor_prop.mk
+++
@@ -1,8 +0,0 @@
-# Sensors
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.sensors.noneui=true \
-    ro.xiaomi.sar_fusion=true
-
-# Display
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.display.disable_avr=1
