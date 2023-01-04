13e30fe84968ef280baa18c061a3beb95e4d0a29
Michael Bestas <mkbestas@lineageos.org>
2023-01-04 09:09:36 +0200

    chiron: Switch to OSS libnotifyaudiohal
    Change-Id: Id392223e539716522d1c54efecae38ef72e38a1d

--- device.mk
+++ device.mk
@@ -35,5 +35,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Sensors
+PRODUCT_PACKAGES += \
+    libnotifyaudiohal:64
+
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/chiron/chiron-vendor.mk)
